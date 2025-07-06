5b72d6e0595ecfd02952f65e803b284efffe4769
toan_nguyen <nguyenminhtoan451999@gmail.com>
2025-07-06 20:36:21 +0700

    fix: fix color font features

--- components/client/features.tsx
+++ components/client/features.tsx
@@ -8,7 +8,7 @@ export const Features = () => (
           <h2 className="text-3xl md:text-5xl tracking-tighter max-w-xl font-regular text-left">
             Có gì mới
           </h2>
-          {/* <p className="text-lg max-w-xl lg:max-w-lg leading-relaxed tracking-tight text-muted-foreground  text-left">
+          {/* <p className="text-lg max-w-xl lg:max-w-lg leading-relaxed tracking-tight  text-left">
             Managing a small business today is already tough.
           </p> */}
         </div>
@@ -17,8 +17,8 @@ export const Features = () => (
         <div className="bg-muted rounded-md h-full lg:col-span-2 p-6 aspect-square lg:aspect-auto flex justify-between flex-col">
           <User className="w-8 h-8 stroke-1" />
           <div className="flex flex-col">
-            <h3 className="text-xl tracking-tight">Pay supplier invoices</h3>
-            <p className="text-muted-foreground max-w-xs text-base">
+            <h3 className="text-xl tracking-tight font-medium">Pay supplier invoices</h3>
+            <p className="max-w-xs text-base">
               Our goal is to streamline SMB trade, making it easier and faster
               than ever.
             </p>
@@ -27,8 +27,8 @@ export const Features = () => (
         <div className="bg-muted rounded-md  aspect-square p-6 flex justify-between flex-col">
           <User className="w-8 h-8 stroke-1" />
           <div className="flex flex-col">
-            <h3 className="text-xl tracking-tight">Pay supplier invoices</h3>
-            <p className="text-muted-foreground max-w-xs text-base">
+            <h3 className="text-xl tracking-tight font-medium">Pay supplier invoices</h3>
+            <p className="max-w-xs text-base">
               Our goal is to streamline SMB trade, making it easier and faster
               than ever.
             </p>
@@ -38,8 +38,8 @@ export const Features = () => (
         <div className="bg-muted rounded-md aspect-square p-6 flex justify-between flex-col">
           <User className="w-8 h-8 stroke-1" />
           <div className="flex flex-col">
-            <h3 className="text-xl tracking-tight">Pay supplier invoices</h3>
-            <p className="text-muted-foreground max-w-xs text-base">
+            <h3 className="text-xl tracking-tight font-medium">Pay supplier invoices</h3>
+            <p className="max-w-xs text-base">
               Our goal is to streamline SMB trade, making it easier and faster
               than ever.
             </p>
@@ -48,8 +48,8 @@ export const Features = () => (
         <div className="bg-muted rounded-md h-full lg:col-span-2 p-6 aspect-square lg:aspect-auto flex justify-between flex-col">
           <User className="w-8 h-8 stroke-1" />
           <div className="flex flex-col">
-            <h3 className="text-xl tracking-tight">Pay supplier invoices</h3>
-            <p className="text-muted-foreground max-w-xs text-base">
+            <h3 className="text-xl tracking-tight font-medium">Pay supplier invoices</h3>
+            <p className="max-w-xs text-base">
               Our goal is to streamline SMB trade, making it easier and faster
               than ever.
             </p>
